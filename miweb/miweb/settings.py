9cd7a2ac4e0bebad3a75aa1b43cc4d358a3ccaa0
Ignacio-Olivares1 <135654609+Ignacio-Olivares1@users.noreply.github.com>
2025-05-31 18:07:43 +0000

    modificando css

--- miweb/miweb/settings.py
+++ miweb/miweb/settings.py
@@ -57,5 +57,10 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
-STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
