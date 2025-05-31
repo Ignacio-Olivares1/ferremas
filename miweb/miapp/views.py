dca00734f6ad98ca5cb62cd97ea25ed05cf55b98
Ignacio-Olivares1 <135654609+Ignacio-Olivares1@users.noreply.github.com>
2025-05-31 18:45:39 +0000

    css implementado

--- miweb/miapp/views.py
+++ miweb/miapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-def home(request):
+def inicio(request):
     return render(request, 'miapp/index.html')
--- miweb/miweb/urls.py
+++ miweb/miweb/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from . import views
+from miapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
